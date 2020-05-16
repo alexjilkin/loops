6c94f089fcda46d04bf6dd192e6f1b2ee30037f3
alexj <jilkinalex@gmail.com>
2020-05-16 18:41:11 +0300

    Use Delay for jsynth

--- web/src/components/Amp.jsx
+++ web/src/components/Amp.jsx
@@ -1,18 +1,18 @@
 import React, {useRef, useEffect, useCallback, useState} from 'react'
 import {Amp} from '@loops/core'
-import {Delay} from '@loops/core'
+import {useDelay} from '@jsynth/core/modules'
 const delay = new Delay()
 
 import './Amp.scss'
 
 export default ({loopsEngine, inputId}) => {
-
     const [isOn, setIsOn] = useState(false)
     const [amp] = useAmp(inputId)
+    const [delayTransform, frequency, setFrequency] = useDelay()
 
     useEffect(() => {
       loopsEngine.addMiddleware(amp.getTransferFunction())
-      loopsEngine.addMiddleware(delay.transferFunc)
+      loopsEngine.addMiddleware(delayTransform)
     }, [])
 
     const handleToggle = useCallback(() => {
